d6af94318578ae73c025d9733d682b6ff9c72bc6
Marco Formoso <marco.a.formoso@gmail.com>
2015-12-30 22:08:31 +0100

    Added bluetooth for Android

--- bluet.py
+++ bluet.py
@@ -1,7 +1,35 @@
-import bluetooth
+from kivy.utils import platform
+
+if 'android' == platform:
+    from jnius import autoclass, cast
+
+    BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
+    BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
+    BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
+    PythonActivity = autoclass('org.renpy.android.PythonActivity')
+    Intent = autoclass('android.content.Intent')
+
+    UUID = autoclass('java.util.UUID')
+    android = True
+
+    bt_adapter = BluetoothAdapter.getDefaultAdapter()
+    if not bt_adapter.isEnabled():
+        intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
+        currentActivity.startActivityForResult(intent, 1)
+
+
+else:
+    android = False
+    import bluetooth
+
 
 
 def discover():
-    return bluetooth.discover_devices()
+    if android:
+        return [x.getName() for x in bt_adapter.getBondedDevices().toArray()]
+
+    else:
+        return bluetooth.discover_devices()
 
 
